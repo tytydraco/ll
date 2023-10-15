25e59b52e3ce5d8d67811f2e1952a8701b7b239c
Tyler Nijmeh <tylernij@gmail.com>
2023-10-14 18:54:51 -0700

    Change spacing for pie chart
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/ui/details/terpene_chart.dart
+++ lib/src/ui/details/terpene_chart.dart
@@ -27,7 +27,7 @@ class _TerpeneChartState extends State<TerpeneChart> {
     return PieChartSectionData(
       showTitle: false,
       color: color,
-      badgePositionPercentageOffset: 2,
+      badgePositionPercentageOffset: -1,
       badgeWidget: Text(
         terpene.capitalize(),
         textAlign: TextAlign.center,
@@ -49,7 +49,6 @@ class _TerpeneChartState extends State<TerpeneChart> {
           aspectRatio: 1,
           child: PieChart(
             PieChartData(
-              centerSpaceRadius: 40,
               borderData: FlBorderData(
                 show: false,
               ),
